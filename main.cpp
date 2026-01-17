94a6b6ad024d79be974c9600ec046356a0e18613
leonardo9119ah4 <leonardoartursasse@gmail.com>
2026-01-17 17:45:24 -0300

    cpp-httplib

--- main.cpp
+++ main.cpp
@@ -1,59 +1,13 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <cstring>
+#include "httplib.h"
+using namespace std;
+using namespace httplib;
 
-int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[30000] = {0};
-
-    // Cria socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Configura socket
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
-
-    // Bind e listen
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    listen(server_fd, 3);
-    std::cout << "Servidor escutando em http://localhost:8080" << std::endl;
-
-    while (true) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
-
-        read(new_socket, buffer, 30000);
-        std::cout << "Requisição: " << buffer << std::endl;
-
-        // Lê arquivo estático
-        std::ifstream file("index.html", std::ios::binary | std::ios::ate);
-        std::streamsize size = file.tellg();
-        file.seekg(0, std::ios::beg);
-        std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-
-        // Resposta HTTP
-        std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + std::to_string(content.size()) + "\r\n\r\n" + content;
-
-        send(new_socket, response.c_str(), response.size(), 0);
-        close(new_socket);
-    }
-    close(server_fd);
+int main(){
+    Server svr;
+    svr.Get("/", [](const Request& req, Response& res){
+        res.set_content("Hello World!", "text/plain");
+    });
+    auto ret = svr.set_mount_point("/", "./public");
+    svr.listen("localhost", 8080);
     return 0;
 }
